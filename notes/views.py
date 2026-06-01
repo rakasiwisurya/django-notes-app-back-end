36c4fdbddcb450aff7775e54fdad7d40f8dd8d53
Rakasiwi Surya <rakasiwi@medhubarc.com>
2026-06-02 02:04:51 +0700

    feat:Mengimplementasi Kriteria 2

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -13,3 +14,22 @@ class NoteList(APIView):
             return Response(note.data, status=status.HTTP_201_CREATED)
  
         return Response(note.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True, context={'request': request})  # Pass request context
+        return Response({
+            "notes": serializer.data
+        }, status=status.HTTP_200_OK)
+
+class NoteDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
