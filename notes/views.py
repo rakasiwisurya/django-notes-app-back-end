32d0934da765527d81eb5930ba29d288036b5cce
Rakasiwi Surya <rakasiwi@medhubarc.com>
2026-06-02 02:18:03 +0700

    feat:Mengimplementasi Kriteria 4

--- notes/views.py
+++ notes/views.py
@@ -41,3 +41,8 @@ class NoteDetail(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        note = self.get_object(pk)
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
